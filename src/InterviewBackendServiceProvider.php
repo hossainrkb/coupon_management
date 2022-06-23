0ba866068f7a5db393429710badff4d42c72f551
Rakib Hosen <rakib.151045@gmail.com>
2022-06-23 20:04:11 +0600

    shrinking route files

--- src/InterviewBackendServiceProvider.php
+++ src/InterviewBackendServiceProvider.php
@@ -8,7 +8,7 @@ class InterviewBackendServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/api.php');
 
         $this->loadMigrationsFrom(__DIR__ . '/../database/Migrations');
     }
--- src/Routes/api.php
+++ src/Routes/api.php
@@ -1,25 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-// Beginning Of HelloApiController Routes
-
-use BTAL\XYZ\ABC\Http\Controllers\Api\HelloApiController;
-
-Route::prefix('hellos')->group(function () {
-    Route::prefix('api')->group(function () {
-
-        Route::get('/', [HelloApiController::class, 'index']);
-        Route::get('/view/{id}', [HelloApiController::class, 'view']);
-        Route::get('/edit/{id}', [HelloApiController::class, 'edit']);
-
-        Route::post('/store', [HelloApiController::class, 'store']);
-        Route::post('/update/{id}', [HelloApiController::class, 'update']);
-
-        Route::get('/delete/{id}', [HelloApiController::class, 'delete']);
-    });
-});
-
-// End Of HelloApiController Routes
-
-
