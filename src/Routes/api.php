4fe0525b4cd7263a29f57ce25fd82e221dceba1e
Rakib Hosen <rakib.151045@gmail.com>
2022-06-24 12:10:08 +0600

    update api for coupon

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -5,10 +5,11 @@ use Interview\Backend\Http\Controllers\AppliedableCouponController;
 use Interview\Backend\Http\Controllers\CouponController;
 use Interview\Backend\Http\Controllers\CourseController;
 
-Route::group(['prefix' => 'api', 'middleware' => 'auth:api'], function () {
+Route::group(['prefix' => 'api', 'middleware' => ['auth:api','bindings']], function () {
     Route::group(['prefix' => 'coupons'], function () {
         Route::post("/", [CouponController::class, 'index']);
         Route::post("/store", [CouponController::class, 'store']);
+        Route::post("/show/{coupon}", [CouponController::class, 'show']);
     });
     Route::group(['prefix' => 'applied-coupon'], function () {
         Route::post("category/{category}", [AppliedableCouponController::class, 'appliedCouponOnCategory']);
